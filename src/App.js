ead0df0a2604fb3676d46e6b19c414a8c164156f
Carson 723 <23cadememy@gmail.com>
2022-09-21 09:16:18 -0500

    Updated App.js + Created files for CreatePost.css and CreatePost.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import DisplayPost from './Components/DisplayPost/DisplayPost';
-import CreatePost from './Components/DisplayPost/CreatePost/CreatePost';
+import CreatePost from './Components/CreatePost/CreatePost';
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
 
 function App() {
 
-  const [entries, setEntries] = useState([{name: 'name test', post: 'post test'}])
+  const [posts, setPosts] = useState([{userName: 'name test', comment: "post test"}])
 
-  function addNewPost(entry) {
-    let tempPosts = [entry, ...entries];
-    setEntries(tempPosts);
+  function addNewPost (newPost){
+    let tempPosts = [...posts, newPost];
+    
+    setPosts(tempPosts);
+    console.log(tempPosts);
   }
-
   return (
     <div>
-      <DisplayPost parentEntries={entries} />
-      <CreatePost addNewPost={addNewPost}/>
+      <NavBar />
+      <CreatePost addNewPostProperty={addNewPost} />
+      <DisplayPost xyzposts={posts}/>
     </div>
   );
 }
