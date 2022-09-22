183f61fe5d4dd866ff81f34def84f1a51173f892
Carson 723 <23cadememy@gmail.com>
2022-09-22 09:07:35 -0500

    Completed NavBar.jsx

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+// import './NavBar.css';
+
+const NavBar = (props) => {
+    return ( 
+        <div className='container-fluid'>
+            <div className='border-box'>
+                <h3 className='header-title'>Social
+                <small className='text-muted'>Feed</small></h3>
+            </div>
+        </div>
+     );
+}
+ 
+export default NavBar;
